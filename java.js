dd4bb26928be6f89fed45a8700bb5055f79137af
kmlproduct <kam.salaheddine@gmail.com>
2019-12-07 23:57:56 +0100

    new repository

---
+++ java.js
@@ -0,0 +1,9 @@
+var myName = "salah";
+var myage  = 30 ;
+
+
+document.getElementById("salah").innerHTML = 
+"<span style='color:red'> ma name is</span>: "myName" 
+" <span style='color:red'> my age is</span>: " + "<span style='color:blue'>" + myage "</span>"  ;
+
+
